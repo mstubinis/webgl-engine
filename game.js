1c517b98a977e9fcb5eaadc20dc3d009650b81ad
Mike Tubinis <michaeltubinis@gmail.com>
2016-07-24 00:37:43 -0400

    directory refactor and lighting fixes

--- game.js
+++ game.js
@@ -3,24 +3,24 @@ var Engine = Engine || {};
     'use strict'; Engine.Game = {};
     
     Engine.Game.initResources = function(){
-        var defiantMesh = new Mesh("Defiant","models/defiant.obj");
-        var defiantMaterial = new Material("Defiant","models/defiant.jpg","models/defiant_Glow.jpg","models/defiant_Normal.jpg");
+        var defiantMesh = new Mesh("Defiant","data/models/defiant.obj");
+        var defiantMaterial = new Material("Defiant","data/models/defiant.png","data/models/defiant_Glow.png","data/models/defiant_Normal.png");
     }
     Engine.Game.initLogic = function(){
         var files = [
-            "skyboxes/SolarSystem/Front.jpg",
-            "skyboxes/SolarSystem/Back.jpg",
-            "skyboxes/SolarSystem/Left.jpg",
-            "skyboxes/SolarSystem/Right.jpg",
-            "skyboxes/SolarSystem/Top.jpg",
-            "skyboxes/SolarSystem/Bottom.jpg"
+            "data/skyboxes/SolarSystem/Front.jpg",
+            "data/skyboxes/SolarSystem/Back.jpg",
+            "data/skyboxes/SolarSystem/Left.jpg",
+            "data/skyboxes/SolarSystem/Right.jpg",
+            "data/skyboxes/SolarSystem/Top.jpg",
+            "data/skyboxes/SolarSystem/Bottom.jpg"
         ];
         var skybox = new Skybox("SceneSkybox",files);
         var defiant = new GameObject("Defiant","Defiant","Defiant");
         defiant.setPosition(0,0,0);
         
         var light = new Light("DirLight1");
-		light.setPosition(5,5,5);
+		light.setPosition(2,2,2);
 
         Engine.requestPointerLock();
     }
@@ -46,25 +46,5 @@ var Engine = Engine || {};
         }
         Engine.camera.rotateX(Engine.EventManager.mouse.diffY*0.0025);
         Engine.camera.rotateY(-Engine.EventManager.mouse.diffX*0.0025);
-        /*
-        if(Engine.isKeyDown("KEY_F1")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].constant -= 0.001;}
-        }
-        if(Engine.isKeyDown("KEY_F2")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].constant += 0.001;}
-        }
-        if(Engine.isKeyDown("KEY_F3")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].linear -= 0.001;}
-        }
-        if(Engine.isKeyDown("KEY_F4")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].linear += 0.001;}
-        }
-        if(Engine.isKeyDown("KEY_F5")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].exponent -= 0.001;}
-        }
-        if(Engine.isKeyDown("KEY_F6")){
-            for(var key in Engine.ResourceManager.lights){Engine.ResourceManager.lights[key].exponent += 0.001;}
-        }
-        */
     }
 })(this);
