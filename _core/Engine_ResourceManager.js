6c5984236d76dc86e35eb3997c36a817e8741495
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-15 13:58:37 -0400

    Update Engine_ResourceManager.js

--- _core/Engine_ResourceManager.js
+++ _core/Engine_ResourceManager.js
@@ -2,15 +2,15 @@
 var Engine = Engine || {};
 (function (scope, undefined){
     Engine.ResourceManager = {};
-        
+
     Engine.ResourceManager.shaders = {};
     Engine.ResourceManager.meshes = {};
     Engine.ResourceManager.materials = {};
     Engine.ResourceManager.textures = {};
     Engine.ResourceManager.sounds = {};
-    
+
     Engine.ResourceManager.scenes = {};
-    
+
     Engine.ResourceManager.initPreGameResources = function(){//add mandatory game resources
         new Shader("Default","vshader","fshader",["position","uv","normal","binormal","tangent"]);
     }
@@ -19,7 +19,7 @@ var Engine = Engine || {};
             new Scene('DefaultScene');
         }
     }
-    
+
     Engine.ResourceManager.initDefaultResources = function(){//add any needed game resources that the user might have forgotten
         if(!("Plane" in Engine.ResourceManager.meshes)){
             var data = "v -1.000000 -1.000000 0.000000\n"+
