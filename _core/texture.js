1edea020186002f5df33e82506ad77a2ca70f345
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-15 13:51:56 -0400

    Update texture.js

--- _core/texture.js
+++ _core/texture.js
@@ -13,38 +13,38 @@ var Texture = function(name,file){
         gl.texParameteri(gl.TEXTURE_CUBE_MAP,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_CUBE_MAP,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_CUBE_MAP,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE); 
-     
+
         var faces = [gl.TEXTURE_CUBE_MAP_POSITIVE_Z,gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
                      gl.TEXTURE_CUBE_MAP_POSITIVE_X,gl.TEXTURE_CUBE_MAP_POSITIVE_Y,gl.TEXTURE_CUBE_MAP_NEGATIVE_Y];
-        
+
         var images = {};
         for(var i = 0; i < faces.length; i++){
             var ext = Engine.getExtension(file[i]);
             var buffType = gl.RGBA;
             if(ext == "jpg" || ext == "jpeg") buffType = gl.RGB;
             gl.texImage2D(faces[i], 0, buffType, 1, 1, 0,buffType, gl.UNSIGNED_BYTE, new Uint8Array([0,0,0,255]));
-			images["_" + i] = -1;
-			images["_" + i + "face"] = -1;
+            images["_" + i] = -1;
+            images["_" + i + "face"] = -1;
         }
         for(var i = 0; i < faces.length; i++){
             var img = new Image();
             var face = faces[i];
-			var ext = Engine.getExtension(file[i]);
+            var ext = Engine.getExtension(file[i]);
             img.src = file[i];
             img.onload = function(_images,_i,_this,_face,_img,_ext) {
                 return function(){
                     var buffType = gl.RGBA;
                     if(_ext == "jpg" || _ext == "jpeg") buffType = gl.RGB;
-                    
+
                     _images["_" + _i] = _img;
                     _images["_" + _i + "face"] = _face;
                     var _loaded = true; for(var key in _images){ if(_images[key] == -1){ _loaded = false; } }
                     if(_loaded){for(var j = 0; j < faces.length; j++){
-						gl.bindTexture(gl.TEXTURE_CUBE_MAP, _this.texture);
-						gl.texImage2D(_images["_" + j + "face"], 0, buffType, buffType, gl.UNSIGNED_BYTE, _images["_" + j]);
-					}
-					//cleanup
-					for (var member in _images) delete _images[member];}
+                        gl.bindTexture(gl.TEXTURE_CUBE_MAP, _this.texture);
+                        gl.texImage2D(_images["_" + j + "face"], 0, buffType, buffType, gl.UNSIGNED_BYTE, _images["_" + j]);
+                    }
+                    //cleanup
+                    for (var member in _images) delete _images[member];}
                 }
             }(images,i,_this,face,img,ext);
         }
