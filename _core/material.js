51d2b12bdca8c1212f9c930a1d372ea2b721c24f
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-15 13:55:08 -0400

    Update material.js

--- _core/material.js
+++ _core/material.js
@@ -9,13 +9,13 @@ var Material = function(name,diffuseFile,glowFile,normalFile){
         this.glow = new Texture(glowFile,glowFile);
     if(normalFile !== undefined && normalFile !== "")
         this.normal = new Texture(normalFile,normalFile);
-    
+
     this.color = vec3.fill(1.0,1.0,1.0);
     this.specular = vec3.fill(1.0,1.0,1.0);
     this.shininess = 50.0;
     this.shadeless = false;
     this.transparency = 1.0;
-    
+
     Engine.ResourceManager.materials[name] = this;
 }; 
 Material.prototype.sendUniforms = function(program){
@@ -34,7 +34,7 @@ Material.prototype.sendUniforms = function(program){
         gl.bindTexture(gl.TEXTURE_2D, this.normal.texture);
         gl.uniform1i(gl.getUniformLocation(program, "normalMap"), 2);
     }
-    
+
     gl.uniform1i(gl.getUniformLocation(program, "MaterialShadeless"), (this.shadeless ? 1 : 0));
     gl.uniform4f(gl.getUniformLocation(program, "MaterialColor"), this.color[0],this.color[1],this.color[2],this.transparency);
     gl.uniform4f(gl.getUniformLocation(program, "MaterialSpecularity"), this.specular[0],this.specular[1],this.specular[2],this.shininess);
